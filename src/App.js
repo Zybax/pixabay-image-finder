2b9324680689a4f791b0479a981c465b5b965720
Oliver Lluberes <olimail47@gmail.com>
2018-06-01 09:59:18 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Search from './components/Search/Search';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div>
+          <Navbar />
+          <Search/ >
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
